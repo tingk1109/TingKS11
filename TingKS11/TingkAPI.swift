b6cd908cc4317057594a5eb5ea50801d08f29326
user <user@userde-MacBook-2.local>
2017-06-19 10:11:15 +0800

    v2

---
+++ TingKS11/TingkAPI.swift
@@ -0,0 +1,30 @@
+//
+//  TingkAPI.swift
+//  TingKS11
+//
+//  Created by user on 2017/6/19.
+//  Copyright © 2017年 user. All rights reserved.
+//
+
+import Foundation
+
+class Brad01{
+    var x = 1
+    func m1(){
+        print("Brad01:m1()x = \(x)")
+    }
+}
+
+class Rect{
+    var x:Int = 0
+    var y:Int = 0
+    var w:Int = 0
+    var h:Int = 0
+}
+
+struct Brad02{
+    var x = 1
+    func m1(){
+        print("Brad01:m1()x = \(x)")
+    }
+}
--- TingKS11/main.swift
+++ TingKS11/main.swift
@@ -8,5 +8,76 @@
 
 import Foundation
 
+//屬性
+extension Brad01{
+    var y:Int{return x * 2}
+}
+
+var Obj1 = Brad01()
+print(Obj1.y)
+
+extension Double{
+    var m: Double{return self}
+    var km: Double{return self * 1000.0}
+    var cm: Double{return self / 100.0}
+}
+var len = 123.0
+print(len.m)
+print(len.km)
+print(len.cm)
+var len2 = 100.0
+var len3 = 1.km + 4.cm
+print(len3.m)
+
+extension Rect{
+    var cx:Int{return x + w / 2}
+    var cy:Int{return y + h / 2}
+}
+
+extension Brad01{
+    convenience init (x:Int){
+        self.init()
+        self.x = x
+    }
+}
+var Obj2 = Brad01(x:4)
+print(Obj2.x)
+var Obj3 = Brad01()
+print(Obj3.x)
+
+extension Brad01{
+    func m2(){
+        x += 4
+        print("Brad01:m2()x = \(x)")
+    }
+}
+var Obj4 = Brad01()
+Obj4.m2()
+
+extension Brad02 {
+    mutating func m2(){ //結構如要更改值需加 mutating
+        x += 4
+        print("Brad02:m2()x = \(x)")
+    }
+}
+var Obj5 = Brad02()
+Obj5.m2()
+
+extension Int{
+    subscript(d:Int)->Int{
+        var x1 = 1
+        for _ in 0...d{
+            x1 *= 10
+        }
+        var x2 = 1
+        for _ in 0..<d{
+            x2 *= 10
+        }
+        let x3 = self % x1
+        let ret = x3 / x2
+        return ret
+    }
+}
+print(172[0])
 
 
